e059cd6683a1abcd083f1911cb1d026e10d893ef
Soul Queen <89896200+ImSoulQueen@users.noreply.github.com>
2021-09-02 13:09:16 +0530

    Create __init__.py

---
+++ MusicTamilBot/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from MusicTamilBot.services.queues.queues import clear 
+from MusicTamilBot.services.queues.queues import get
+from MusicTamilBot.services.queues.queues import is_empty
+from MusicTamilBot.services.queues.queues import put
+from MusicTamilBot.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
