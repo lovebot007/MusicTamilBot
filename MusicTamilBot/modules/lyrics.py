39021833cd3a4f7c09259a07e440c3f163e85d03
Soul Queen <89896200+ImSoulQueen@users.noreply.github.com>
2021-09-04 11:03:38 +0530

    Update lyrics.py

--- MusicTamilBot/modules/lyrics.py
+++ MusicTamilBot/modules/lyrics.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2021 TeamDaisyX
+# Copyright (C) 2021 ImSoulQueen
 
 
-# This file is part of Daisy (Telegram Bot)
+# This file is part of MusicTamil (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
